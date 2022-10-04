f089f4982faa2fdaabb52e4dfef0f1f45deb58f6
hussaiini <hussaiini@yahoo.com>
2022-10-04 09:57:08 +0100

    full Web3

--- js/index.js
+++ js/index.js
@@ -1,7 +1,5 @@
 'use strict';
 
-
-
 /**
  * add event on element
  */
@@ -14,44 +12,40 @@ const addEventOnElem = function (elem, type, callback) {
   } else {
     elem.addEventListener(type, callback);
   }
-}
-
-
+};
 
 /**
  * toggle navbar
  */
 
-const navbar = document.querySelector("[data-navbar]");
-const navLinks = document.querySelectorAll("[data-nav-link]");
-const navToggler = document.querySelector("[data-nav-toggler]");
+const navbar = document.querySelector('[data-navbar]');
+const navLinks = document.querySelectorAll('[data-nav-link]');
+const navToggler = document.querySelector('[data-nav-toggler]');
 
 const toggleNavbar = function () {
-  navbar.classList.toggle("active");
-  navToggler.classList.toggle("active");
-}
+  navbar.classList.toggle('active');
+  navToggler.classList.toggle('active');
+};
 
-addEventOnElem(navToggler, "click", toggleNavbar);
+addEventOnElem(navToggler, 'click', toggleNavbar);
 
 const closeNavbar = function () {
-  navbar.classList.remove("active");
-  navToggler.classList.remove("active");
-}
+  navbar.classList.remove('active');
+  navToggler.classList.remove('active');
+};
 
-addEventOnElem(navLinks, "click", closeNavbar);
-
-
+addEventOnElem(navLinks, 'click', closeNavbar);
 
 /**
  * header active
  */
 
-const header = document.querySelector("[data-header]");
+const header = document.querySelector('[data-header]');
 
-window.addEventListener("scroll", function () {
+window.addEventListener('scroll', function () {
   if (window.scrollY > 100) {
-    header.classList.add("active");
+    header.classList.add('active');
   } else {
-    header.classList.remove("active");
+    header.classList.remove('active');
   }
 });
